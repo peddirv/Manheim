9040a2759d6c7b5a3ccdeb786b5f6cfdd0ecc75d
Ravi Peddi <ravi.peddi@autotrader.com>
2016-01-13 11:09:48 -0500

    Adding Comments and report softAssert

--- src/test/java/com/manheim/shoestore/Story1.java
+++ src/test/java/com/manheim/shoestore/Story1.java
@@ -1,4 +1,16 @@
 package com.manheim.shoestore;
+/**
+ * Created by rpeddi on 1/12/2016.
+ *
+ * Story 1: Monthly display of new releases
+ * In order to view upcoming shoes being released every month As a user of the Shoe store I want to be able to visit a link for each month and see the shoes being released
+ *
+ * Acceptance Criteria
+ *
+ * Month should display a small Blurb of each shoe
+ * Month should display an image each shoe being released
+ *  Each shoe should have a suggested price pricing
+ */
 
 import com.manheim.shoestore.pages.HomePage;
 import com.manheim.shoestore.pages.Month;
@@ -7,14 +19,11 @@ import java.text.DateFormatSymbols;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
-/**
- * Created by rpeddi on 1/12/2016.
- */
-// created By rpeddi ON DATE 1/12/2016
 
 public class Story1 extends TestExtension {
-
+    private SoftAssert softAssert = new SoftAssert();
     //~ Methods --------------------------------------------------------------------------------------------------------
 
     /**
@@ -63,5 +72,6 @@ public class Story1 extends TestExtension {
         } else {
             System.out.println("No Inventory");
         }
+        months.assertAll();
     }
 }
